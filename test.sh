d1b600773fbe41ccab984ffb1ff4421808aac3c5
Victor Bernier <victor.d.bernier@gmail.com>
2025-02-25 20:16:45 +0100

    correction du test

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 URL="http://www.example.org"
 
 # Effectuer la requête avec curl et récupérer le code de retour HTTP
-HTTP_CODE=$(curl -G -s -o /dev/null --write-out "{response_code}" "$URL")
+HTTP_CODE=$(curl -G -s -o /dev/null --write-out "%{response_code}" "$URL")
 
 echo "$HTTP_CODE"
