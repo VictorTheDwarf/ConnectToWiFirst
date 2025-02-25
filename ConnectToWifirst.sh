0e8283a812f4fe4b780cae233ee0403226f6aaf7
Victor Bernier <victor.d.bernier@gmail.com>
2025-02-25 20:22:02 +0100

    la suite

--- ConnectToWifirst.sh
+++ ConnectToWifirst.sh
@@ -1,7 +1,7 @@
 URL="http://www.example.org"
 
 # Effectuer la requête avec curl et récupérer le code de retour HTTP
-HTTP_CODE=$(curl -G -s -o /dev/null --write-out "{response_code}" "$URL")
+HTTP_CODE=$(curl -G -s -o /dev/null --write-out "%{response_code}" "$URL")
 
 # Vérifier le code de retour
 if [[ "$HTTP_CODE" -eq 200 || "$HTTP_CODE" -eq 302 ]]; then
--- test.sh
+++ test.sh
@@ -3,4 +3,9 @@ URL="http://www.example.org"
 # Effectuer la requête avec curl et récupérer le code de retour HTTP
 HTTP_CODE=$(curl -G -s -o /dev/null --write-out "%{response_code}" "$URL")
 
-echo "$HTTP_CODE"
+# Vérifier le code de retour
+if [[ "$HTTP_CODE" -eq 200 || "$HTTP_CODE" -eq 302 ]]; then
+    echo "Internet available"
+else
+    echo "Proxy detected"
+fi
