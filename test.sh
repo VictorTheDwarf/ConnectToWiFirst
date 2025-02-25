0f73890fb6570a6478477daf054e85c12866624a
Victor Bernier <victor.d.bernier@gmail.com>
2025-02-25 20:12:17 +0100

    bidouille un peu plus poussée

--- test.sh
+++ test.sh
@@ -1,2 +1,6 @@
-TEST="pouet"
-echo "$TEST"
+URL="http://www.example.org"
+
+# Effectuer la requête avec curl et récupérer le code de retour HTTP
+HTTP_CODE=$(curl -G -s -o /dev/null --write-out "{response_code}" "$URL")
+
+echo "$HTTP_CODE"
