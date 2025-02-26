aa33553e491e2d5d5de116ef5bb91ef8799472fd
Victor Bernier <victor.d.bernier@gmail.com>
2025-02-26 18:34:32 +0100

    test de l'ajout de logs

--- test.sh
+++ test.sh
@@ -6,8 +6,11 @@ HTTP_CODE=$(curl -G -s -o /dev/null --write-out "%{response_code}" "$URL")
 # Vérifier le code de retour
 if [[ "$HTTP_CODE" -eq 200 ]]; then
     echo "Internet available"
+    /usr/bin/logger "Internet available"
 elif [[ "$HTTP_CODE" -eq 302 ]]; then
     echo "Proxy detected"
+    /usr/bin/logger "Proxy detected"
 else
     echo "Internet Failure"
+    /usr/bin/logger "Internet Failure"
 fi
