850fcf84f55d1b72187187422acd2d09275be5f6
Victor Bernier <victor.d.bernier@gmail.com>
2025-02-26 18:17:59 +0100

    petite correction rapide

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ HTTP_CODE=$(curl -G -s -o /dev/null --write-out "%{response_code}" "$URL")
 # Vérifier le code de retour
 if [[ "$HTTP_CODE" -eq 200 ]]; then
     echo "Internet available"
-elif [[ "$HTTP_CODE" -eq 302]]; then
+elif [[ "$HTTP_CODE" -eq 302 ]]; then
     echo "Proxy detected"
 else
     echo "Internet Failure"
